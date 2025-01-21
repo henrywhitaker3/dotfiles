3315d345570062f09fcf232d568e93c11562cf7f
Henry Whitaker <henrywhitaker3@outlook.com>
2025-01-21 20:56:49 +0000

    feat: more nvim

--- common/nvim/lua/plugins/treesitter.lua
+++ common/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,34 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function () 
-	local configs = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+        local configs = require("nvim-treesitter.configs")
 
-	configs.setup({
-		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript", "go" },
-		sync_install = false,
-		highlight = { enable = true },
-		indent = { enable = true },
-		})
-	end
+        configs.setup({
+            ensure_installed = {
+                "c",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "elixir",
+                "heex",
+                "javascript",
+                "html",
+                "typescript",
+                "go",
+                "bash",
+                "json",
+                "yaml",
+                "vue",
+                "gitignore",
+                "dockerfile",
+                "markdown",
+                "css",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
 }
