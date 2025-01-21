b5b6a155bb2ee3c55b4fb6acec6a8824107744c0
Henry Whitaker <henrywhitaker3@outlook.com>
2025-01-21 22:58:32 +0000

    fix: fmt

--- common/nvim/lua/plugins/nvim-lspconfig.lua
+++ common/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,75 +1,97 @@
 local config = function()
-    local lspconfig = require("lspconfig")
+	local lspconfig = require("lspconfig")
 
-    local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+	local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 
-    for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	end
 
-    lspconfig.lua_ls.setup({
-        -- capabilitites = capabilities,
-        -- on_attach = on_attach,
-        settings = {
-            Lua = {
-                diagnostics = {
-                    globals = { "vim" },
-                },
-                workspace = {
-                    library = {
-                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                        [vim.fn.expand("$XDG_CONFIG_HOME") .. "/nvim/lua"] = true,
-                    }
-                },
-            },
-        },
-    })
+	lspconfig.lua_ls.setup({
+		-- capabilitites = capabilities,
+		-- on_attach = on_attach,
+		on_init = function(client)
+			if client.workspace_folders then
+				local path = client.workspace_folders[1].name
+				if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+					return
+				end
+			end
 
-    local luacheck = require("efmls-configs.linters.luacheck")
-    local stylua = require("efmls-configs.formatters.stylua")
+			client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+				runtime = {
+					-- Tell the language server which version of Lua you're using
+					-- (most likely LuaJIT in the case of Neovim)
+					version = "LuaJIT",
+				},
+				-- Make the server aware of Neovim runtime files
+				workspace = {
+					checkThirdParty = false,
+					library = {
+						vim.env.VIMRUNTIME,
+						-- Depending on the usage, you might want to add additional paths here.
+						-- "${3rd}/luv/library"
+						-- "${3rd}/busted/library",
+					},
+					-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+					-- library = vim.api.nvim_get_runtime_file("", true)
+				},
+			})
+		end,
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = { "vim" },
+				}
+			},
+		},
+	})
 
-    lspconfig.efm.setup({
-        filetypes = {
-            "lua",
-        },
-        init_options = {
-            documentFormatting = true,
+	local luacheck = require("efmls-configs.linters.luacheck")
+	local stylua = require("efmls-configs.formatters.stylua")
+
+	lspconfig.efm.setup({
+		filetypes = {
+			"lua",
+		},
+		init_options = {
+			documentFormatting = true,
 			documentRangeFormatting = true,
 			hover = true,
 			documentSymbol = true,
 			codeAction = true,
 			completion = true,
-        },
-        settings = {
-            languages = {
-                lua = { luacheck, stylua },
-            }
-        },
-    })
+		},
+		settings = {
+			languages = {
+				lua = { luacheck, stylua },
+			},
+		},
+	})
 
-    local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
-    vim.api.nvim_create_autocmd("BufWritePost", {
-        group = lsp_fmt_group,
-        callback = function()
-            local efm = vim.lsp.get_clients({ name = "efm" })
+	local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = lsp_fmt_group,
+		callback = function()
+			local efm = vim.lsp.get_clients({ name = "efm" })
 
-            if vim.tbl_isempty(efm) then
-                return
-            end
+			if vim.tbl_isempty(efm) then
+				return
+			end
 
-            vim.lsp.buf.format({ name = "efm" })
-        end
-    })
+			vim.lsp.buf.format({ name = "efm" })
+		end,
+	})
 end
 
 return {
-    "neovim/nvim-lspconfig",
-    config = config,
-    lazy = false,
-    dependencies = {
-        "windwp/nvim-autopairs",
-        "williamboman/mason.nvim",
-        "creativenull/efmls-configs-nvim",
-    }
+	"neovim/nvim-lspconfig",
+	config = config,
+	lazy = false,
+	dependencies = {
+		"windwp/nvim-autopairs",
+		"williamboman/mason.nvim",
+		"creativenull/efmls-configs-nvim",
+	},
 }
