844b6625ae76515c29947ffc71381bc94bf31cdb
Henry Whitaker <henrywhitaker3@outlook.com>
2025-01-22 20:58:21 +0000

    fix: clear breakpoints keymap

--- common/nvim/lua/plugins/dap.lua
+++ common/nvim/lua/plugins/dap.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		local opts = { noremap = true, silent = true }
 		vim.keymap.set("n", "<leader>db", "<cmd>DapToggleBreakpoint<CR>", opts)
-		vim.keymap.set("n", "<leader>db", "<cmd>DapClearBreakpints<CR>", opts)
+		vim.keymap.set("n", "<leader>dcb", "<cmd>DapClearBreakpoints<CR>", opts)
 		vim.keymap.set("n", "<leader>dui", function()
 			local widgets = require("dap.ui.widgets")
 			local sidebar = widgets.sidebar(widgets.scopes)
