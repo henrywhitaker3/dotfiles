01d99753cce66007b1eb819698f4196932c1b0d0
Henry Whitaker <henrywhitaker3@outlook.com>
2025-01-22 16:51:15 +0000

    fix: hide .git folder from tree

--- common/nvim/lua/plugins/nvim-tree.lua
+++ common/nvim/lua/plugins/nvim-tree.lua
@@ -1,23 +1,24 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = {
-        sort = {
-            sorter = "case_sensitive",
-        },
-        view = {
-            width = 30,
-            relativenumber = true,
-        },
-        renderer = {
-            group_empty = true,
-        },
-        filters = {
-            dotfiles = false,
-        },
-    },
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = {
+		sort = {
+			sorter = "case_sensitive",
+		},
+		view = {
+			width = 30,
+			relativenumber = true,
+		},
+		renderer = {
+			group_empty = true,
+		},
+		filters = {
+			dotfiles = false,
+			custom = { "^\\.git" },
+		},
+	},
 }
