e9e2478a4ce8e7273973da4fabddeb4201e3388b
Henry Whitaker <henrywhitaker3@outlook.com>
2025-01-22 00:25:32 +0000

    fix: add mason install config

---
+++ common/nvim/lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,11 @@
+return {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    config = {
+        ensure_installed = {
+           "efm",
+            "lua-language-server",
+            "stylua",
+            "luacheck",
+        }
+    }
+}
