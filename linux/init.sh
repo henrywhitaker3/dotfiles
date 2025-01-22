9839ad5561e6ac8f64e254095d6e0a56c991eb49
Henry Whitaker <henrywhitaker3@outlook.com>
2025-01-22 09:56:45 +0000

    fix: instal firacode font on linux

--- linux/init.sh
+++ linux/init.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 
-sudo apt install -y vim wget gcc ripgrep luarocks zsh stow git fzf terraform
+sudo apt install -y vim wget gcc ripgrep luarocks zsh stow git fzf terraform fonts-firacode
 
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/.local/bin
 
