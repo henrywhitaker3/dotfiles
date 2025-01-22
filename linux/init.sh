2b1150a71b305bccbacbc97f5c37c2da2db8d3d3
Henry Whitaker <henrywhitaker3@outlook.com>
2025-01-22 13:02:53 +0000

    fix: linux terraform install

--- linux/init.sh
+++ linux/init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo apt update && sudo apt install gpg -y
+
+# Install terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg /tmp/hashicorp.key
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
 sudo apt update
 
 sudo apt install -y vim wget gcc ripgrep luarocks zsh stow git fzf terraform fonts-firacode make
@@ -16,6 +22,9 @@ current=$(pwd)
     curl https://go.dev/dl/go1.23.5.linux-amd64.tar.gz -O /tmp/go.tar.gz
     cd /tmp && tar -xzvf go.tar.gz && mv go $HOME/ &&cd $current
 fi
+
+$HOME/go/bin/go install golang.org/x/tools/gopls@latest
+
 
 echo Cleaning default config file
 ./common/clean.sh
