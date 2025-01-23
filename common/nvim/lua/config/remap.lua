c50d5ede2f2ec918aa17637485e5b88c5bc5132d
Henry Whitaker <henrywhitaker3@outlook.com>
2025-01-23 16:52:46 +0000

    fix: add chdir keymap
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

--- common/nvim/lua/config/remap.lua
+++ common/nvim/lua/config/remap.lua
@@ -13,6 +13,7 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
 vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
 vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)
 vim.keymap.set("n", "<leader>sh", ":split<CR>", opts)
+vim.keymap.set("n", "<leader>cd", ":chdir<Space>", opts)
 
 -- Commenting
 vim.api.nvim_set_keymap("i", "<C-/>", "<esc>gcc<S-a>", { noremap = false })
