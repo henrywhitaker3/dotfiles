894020dffc7af1aadcc48b86bdb1305c7f0058ac
Henry Whitaker <henrywhitaker3@outlook.com>
2025-01-23 18:51:34 +0000

    fix: add keymap for GoIfErr
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

--- common/nvim/lua/plugins/gopher.lua
+++ common/nvim/lua/plugins/gopher.lua
@@ -5,7 +5,11 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
 	},
-	-- build = function()
-	-- 	vim.cmd.GoInstallDeps()
-	-- end,
+	build = function()
+		vim.cmd.GoInstallDeps()
+	end,
+	config = function(_, opts)
+		require("gopher").setup(opts)
+		vim.keymap.set("i", "<C-g>e", "<cmd>GoIfErr<CR>", { noremap = true, silent = true })
+	end,
 }
