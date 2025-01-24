130b875ab39e87f25b4fdffceb32b058fbe1c4d0
Henry Whitaker <henrywhitaker3@outlook.com>
2025-01-24 15:05:33 +0000

    fix: autopairs settings
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

--- common/nvim/lua/plugins/nvim-autopairs.lua
+++ common/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,6 +1,10 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
+	opts = {
+		fast_wrap = {},
+		disable_filetype = { "TelescopePrompt", "vim" },
+	},
 	config = function(_, opts)
 		require("nvim-autopairs").setup(opts)
 		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
--- common/nvim/lua/plugins/nvim-ts-autotag.lua
+++ common/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -4,5 +4,4 @@ return {
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 	},
-	config = {},
 }
