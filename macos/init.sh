35e0c45f820a77ddc951de9e11e799ffa8db2b5b
Henry Whitaker <henrywhitaker3@outlook.com>
2025-01-25 20:35:40 +0000

    fix: use full path to brew binary in init.sh script

--- macos/init.sh
+++ macos/init.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 install_homebrew() {
-	which brew >/dev/null
-	if [[ $? -ne 0 ]]; then
+	if [[ ! -f /opt/homebrew/bin/brew ]]; then
 		echo Installing homebrew
 		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	else
@@ -14,67 +13,80 @@ install_ohmyzsh() {
 	if [[ -d "$HOME/.oh-my-zsh" ]]; then
 		echo OhMyZsh already installed
 	else
+		echo Installing OhMyZsh
 		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	fi
 }
 
+brew_install() {
+	/opt/homebrew/bin/brew install "$1"
+}
+
+brew_install_cask() {
+	/opt/homebrew/bin/brew install --cask "$1"
+}
+
+brew_tap() {
+	/opt/homebrew/bin/brew tap "$1"
+}
+
 install_brew_things() {
-	brew install git
-	brew install alt-tab
-	brew install helm
-	brew install age
-	brew install k9s
-	brew install kubectl
-	brew install iterm2
-	brew install --cask bruno
-	brew install pre-commit
-	brew install tor-browser
-	brew install calibre
-	brew install vim
-	brew install go
-	brew install 1password
-	brew tap hashicorp/tap
-	brew install hashicorp/tap/terraform
-	brew install watch
-	brew install orbstack
-	brew install spotify
-	brew install --cask nikitabobko/tap/aerospace
-	brew install sops
-	brew install fzf
-	brew install zsh-autosuggestions
-	brew install go-task/tap/go-task
-	brew install stow
-	brew install ariga/tap/atlas
-	brew install sqlc
-	brew install --cask firefox
-	brew install --cask google-chrome
-	brew install visual-studio-code
-	brew install grep
-	brew install gnu-sed
-	brew install yq
-	brew install jq
-	brew install discord
-	brew install ansible
-	brew install nmap
-	brew install --cask wireshark
-	brew install zsh-syntax-highlighting
-	brew install wget
-	brew install pure
-	brew install ollama
-	brew install pipx
-	brew install findutils
-	brew install ffmpeg
-	brew install mediainfo
-	brew install ripgrep
-	brew install luarocks
-	brew install neovim
-	brew install --cask font-fira-code-nerd-font
-	brew install delve
-	brew install gotests
-	brew install golines
-	brew install npm
-	brew install typescript
-	brew install vscode-langservers-extracted
+	brew_install git
+	brew_install alt-tab
+	brew_install helm
+	brew_install age
+	brew_install k9s
+	brew_install kubectl
+	brew_install iterm2
+	brew_install_cask bruno
+	brew_install pre-commit
+	brew_install tor-browser
+	brew_install calibre
+	brew_install vim
+	brew_install go
+	brew_install 1password
+	brew_tap hashicorp/tap
+	brew_install hashicorp/tap/terraform
+	brew_install watch
+	brew_install orbstack
+	brew_install spotify
+	brew_install_cask nikitabobko/tap/aerospace
+	brew_install sops
+	brew_install fzf
+	brew_install zsh-autosuggestions
+	brew_install go-task/tap/go-task
+	brew_install stow
+	brew_install ariga/tap/atlas
+	brew_install sqlc
+	brew_install_cask firefox
+	brew_install_cask google-chrome
+	brew_install visual-studio-code
+	brew_install grep
+	brew_install gnu-sed
+	brew_install yq
+	brew_install jq
+	brew_install discord
+	brew_install ansible
+	brew_install nmap
+	brew_install_cask wireshark
+	brew_install zsh-syntax-highlighting
+	brew_install wget
+	brew_install pure
+	brew_install ollama
+	brew_install pipx
+	brew_install findutils
+	brew_install ffmpeg
+	brew_install mediainfo
+	brew_install ripgrep
+	brew_install luarocks
+	brew_install neovim
+	brew_install_cask font-fira-code-nerd-font
+	brew_install delve
+	brew_install gotests
+	brew_install golines
+	brew_install npm
+	brew_install typescript
+	brew_install vscode-langservers-extracted
 }
 
 install_iterm_theme() {
