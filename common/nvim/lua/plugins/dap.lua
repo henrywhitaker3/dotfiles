d7f40354175e4be196424e1fbd656da1f5a8ee12
Henry Whitaker <henrywhitaker3@outlook.com>
2025-02-13 00:34:42 +0000

    fix: set icons/colour for dap breakpoints

--- common/nvim/lua/plugins/dap.lua
+++ common/nvim/lua/plugins/dap.lua
@@ -7,5 +7,32 @@ return {
 		vim.keymap.set("n", "<leader>dso", "<cmd>DapStepOver<CR>", opts)
 		vim.keymap.set("n", "<leader>dsi", "<cmd>DapStepInto<CR>", opts)
 		vim.keymap.set("n", "<leader>dt", "<cmd>DapTerminate<CR>", opts)
+
+		vim.api.nvim_set_hl(0, "red", { fg = "#cc122e" })
+		vim.api.nvim_set_hl(0, "blue", { fg = "#3d59a1" })
+		vim.api.nvim_set_hl(0, "green", { fg = "#9ece6a" })
+		vim.api.nvim_set_hl(0, "yellow", { fg = "#FFFF00" })
+		vim.api.nvim_set_hl(0, "orange", { fg = "#f09000" })
+
+		vim.fn.sign_define(
+			"DapBreakpoint",
+			{ text = "•", texthl = "red", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+		)
+		vim.fn.sign_define(
+			"DapBreakpointCondition",
+			{ text = "•", texthl = "blue", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+		)
+		vim.fn.sign_define(
+			"DapBreakpointRejected",
+			{ text = "•", texthl = "orange", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+		)
+		vim.fn.sign_define(
+			"DapStopped",
+			{ text = "•", texthl = "green", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+		)
+		vim.fn.sign_define(
+			"DapLogPoint",
+			{ text = "•", texthl = "yellow", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+		)
 	end,
 }
