e776413cc75e68e5e0cf2dd989c5b44fe1dedaf3
Henry Whitaker <henrywhitaker3@outlook.com>
2025-02-16 15:29:15 +0000

    fix: blink config change

--- common/nvim/lua/plugins/blink.lua
+++ common/nvim/lua/plugins/blink.lua
@@ -26,8 +26,8 @@ return {
 			["<C-n>"] = { "snippet_forward", "fallback" },
 			["<C-p>"] = { "snippet_backward", "fallback" },
 		},
-		sources = {
-			cmdline = {},
+		cmdline = {
+			sources = {},
 		},
 		completion = {
 			ghost_text = { enabled = true },
