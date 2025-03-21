631df24cc77bf4fe583896296b2033acae973bde
Henry Whitaker <henrywhitaker3@outlook.com>
2025-03-21 11:15:19 +0000

    chore: add toggle for light mode theme

--- common/nvim/lua/plugins/theme.lua
+++ common/nvim/lua/plugins/theme.lua
@@ -1,12 +1,29 @@
+local theme = {
+	dark = "catppuccin-macchiato",
+	light = "catppuccin-latte",
+}
+
+local toggle = function()
+	local cases = {}
+	cases[theme.dark] = theme.light
+	cases[theme.light] = theme.dark
+	cases["default"] = theme.dark
+
+	local current = vim.g.colors_name
+	if current == nil then
+		current = "default"
+	end
+
+	vim.cmd("colorscheme " .. cases[current])
+end
+
 return {
 	"catppuccin/nvim",
 	lazy = false,
 	priority = 999,
-	opts = {
-		flavour = "macchiato",
-	},
 	config = function(_, opts)
 		require("catppuccin").setup(opts)
-		vim.cmd("colorscheme catppuccin")
+		toggle()
+		vim.keymap.set("n", "<leader>ct", toggle, { noremap = true, silent = true })
 	end,
 }
