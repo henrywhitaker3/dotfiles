be32913554d02c4f730ade98bb905f0149002e64
Henry Whitaker <henrywhitaker3@outlook.com>
2025-09-21 10:48:09 +0100

    fix: helm/yaml lsp
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

--- common/nvim/lua/config/lsp/helm.lua
+++ common/nvim/lua/config/lsp/helm.lua
@@ -1,5 +1,5 @@
 local server = "helm_ls"
-local filetypes = { "helm" }
+local filetypes = { "helm", "yaml.helm-values" }
 
 return {
 	server = server,
--- common/nvim/lua/config/lsp/yaml.lua
+++ common/nvim/lua/config/lsp/yaml.lua
@@ -1,5 +1,5 @@
 local server = "yamlls"
-local filetypes = { "yaml" }
+local filetypes = { "yaml", "helm", "yaml.helm-values" }
 local config = {
 	settings = {
 		yaml = {
