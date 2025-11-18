c02dd322ce19ee20a2ac8acd721cfbfb58c57736
Henry Whitaker <henry.whitaker@commify.com>
2025-11-18 18:36:18 +0000

    feat: use snacks and get rid of some plugins
    Signed-off-by: Henry Whitaker <henry.whitaker@commify.com>

--- common/nvim/lua/plugins/overseer.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"stevearc/overseer.nvim",
-	event = "VeryLazy",
-	dependencies = { "nvim-telescope/telescope.nvim", "stevearc/dressing.nvim" },
-	config = function(_, opts)
-		require("overseer").setup(opts)
-		vim.keymap.set("n", "<leader>pt", ":OverseerRun<CR>", { noremap = true, silent = true })
-	end,
-}
---
+++ common/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,59 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		lazygit = {
+			configure = true,
+		},
+		picker = {},
+	},
+	keys = {
+		{
+			"<leader>gi",
+			function()
+				Snacks.lazygit.open()
+			end,
+		},
+		{
+			"<C-p>",
+			function()
+				Snacks.picker.files({ layout = "dropdown" })
+			end,
+		},
+		{
+			"<leader>pf",
+			function()
+				Snacks.picker.files({ layout = { preview = false, preset = "vertical" } })
+			end,
+		},
+		{
+			"<leader>pw",
+			function()
+				Snacks.picker.grep_word({ layout = "dropdown" })
+			end,
+		},
+		{
+			"<leader>pb",
+			function()
+				Snacks.picker.buffers({ layout = "dropdown" })
+			end,
+		},
+		{
+			"<leader>po",
+			function()
+				Snacks.picker.lsp_symbols({ layout = "dropdown" })
+			end,
+		},
+		{
+			"<leader>pr",
+			function()
+				Snacks.picker.lsp_references({ layout = "dropdown" })
+			end,
+		},
+		{
+			"<leader>pm",
+			function()
+				Snacks.picker.marks({ layout = "dropdown" })
+			end,
+		},
+	},
+}
