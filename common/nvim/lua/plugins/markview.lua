126e99bffe421c838ec2266daf409b55d691db1e
Henry Whitaker <henrywhitaker3@outlook.com>
2026-02-28 09:54:59 +0000

    fix: add markview plugin
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

---
+++ common/nvim/lua/plugins/markview.lua
@@ -0,0 +1,25 @@
+return {
+	"OXY2DEV/markview.nvim",
+	lazy = false,
+	opts = {
+		preview = {
+			enable = false,
+		},
+	},
+	keys = {
+		{
+			"<leader>mt",
+			function()
+				vim.cmd("Markview Toggle")
+			end,
+			desc = "Toggle the Markview previewer",
+		},
+		{
+			"<leader>mp",
+			function()
+				vim.cmd("Markview splitToggle")
+			end,
+			desc = "Toggle the Markview split previewer",
+		},
+	},
+}
