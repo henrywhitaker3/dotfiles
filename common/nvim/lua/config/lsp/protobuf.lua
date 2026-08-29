208702954ade54ff4759b78def6e7d94737533ae
Henry Whitaker <henrywhitaker3@outlook.com>
2026-08-29 07:23:05 +0100

    fix: dont use proto lsp in other buffers
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

--- common/nvim/lua/config/lsp/protobuf.lua
+++ common/nvim/lua/config/lsp/protobuf.lua
@@ -1,7 +1,14 @@
 local server = "protols"
 local filetypes = { "proto" }
 
+local config = {
+	on_init = function(client)
+		client.server_capabilities.textDocumentSync.save = nil
+	end,
+}
+
 return {
 	server = server,
 	filetypes = filetypes,
+	config = config,
 }
