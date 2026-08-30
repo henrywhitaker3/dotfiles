706f7caf04b6cd22ff71a75dfbd0ba5409d0ef8a
Henry Whitaker <henrywhitaker3@outlook.com>
2026-08-30 15:24:44 +0100

    fix: upbrew script mise env auto
    Signed-off-by: Henry Whitaker <henrywhitaker3@outlook.com>

--- common/brew_upgrade.sh
+++ common/brew_upgrade.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd ~/code/dotfiles/ || exit 1
-mise bootstrap packages upgrade
+MISE_AUTO_ENV=true mise bootstrap packages upgrade
+MISE_AUTO_ENV=true mise up
 brew update && brew upgrade -g
